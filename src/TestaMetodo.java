3a151845b1ae0ade9d402643e6bafba35893026c
paulovieira01 <paulodavidpd@hotmail.com>
2023-05-10 23:20:40 -0300

    aula-oo1

--- src/TestaMetodo.java
+++ src/TestaMetodo.java
@@ -2,7 +2,7 @@ public class TestaMetodo {
     public static void main(String[] args) {
         Conta contaDoPaulo = new Conta();
         contaDoPaulo.saldo =100;
-        contaDoPaulo.deposita(50);
+        contaDoPaulo.deposita(100);
         System.out.println("Minha conta tem " + contaDoPaulo.saldo);
     }
 }
