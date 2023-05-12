1964851b4f8c1e00b7e6309900f6e30c64878263
paulovieira01 <paulodavidpd@hotmail.com>
2023-05-11 21:49:08 -0300

    aplicando void, if, else, booelan

--- src/TestaMetodo.java
+++ src/TestaMetodo.java
@@ -1,8 +1,21 @@
 public class TestaMetodo {
     public static void main(String[] args) {
         Conta contaDoPaulo = new Conta();
-        contaDoPaulo.saldo =100;
+        contaDoPaulo.saldo = 100;
         contaDoPaulo.deposita(100);
         System.out.println("Minha conta tem " + contaDoPaulo.saldo);
+        System.out.println(contaDoPaulo.saldo);
+                contaDoPaulo.saca(20);
+        System.out.println(contaDoPaulo.saldo);
+
+        Conta contaDaJasmine = new Conta();
+        contaDaJasmine.deposita(3000);
+
+        contaDaJasmine.transfere(1500, contaDoPaulo);
+        System.out.println(contaDaJasmine.saldo);
+        System.out.println(contaDoPaulo.saldo );
+
+
+
     }
 }
